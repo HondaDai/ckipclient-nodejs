f399f0d0eccda3dc3c2c70787c7736655691c8d9
onlinemad-desktop-ubuntu <onlinemad@gmail.com>
2012-12-10 00:39:16 +0800

    Basic implementation.

---
+++ test/ckipclient.send.js
@@ -0,0 +1,9 @@
+var ckipclient = require('../');
+
+describe('ckipclient', function(){
+  describe('.send()', function(){
+    it('should send text to CKIP server', function(done){
+		ckipclient.send();
+    })
+  })
+})
