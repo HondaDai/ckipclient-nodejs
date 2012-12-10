c66898b8dfdddf8429b67d2925b6197660e8ee69
Ian Wu - IBM <onlinemad@gmail.com>
2012-12-10 16:41:05 +0800

    Working on send() function.

--- test/ckipclient.send.js
+++ test/ckipclient.send.js
@@ -1,9 +1,10 @@
-var ckipclient = require('../');
+var CKIPClient = require('../');
 
 describe('ckipclient', function(){
-  describe('.send()', function(){
-    it('should send text to CKIP server', function(done){
-		ckipclient.send();
-    })
-  })
+	describe('.send()', function(){
+		var client = new CKIPClient('127.0.0.1', 9999, 'user', 'pwd');
+		it('should send text to CKIP server', function(done){
+			client.send('台新金控12月3日將召開股東臨時會進行董監改選。');
+		})
+	})
 })
