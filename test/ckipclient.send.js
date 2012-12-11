3e93fa51915ba2ad277e71bea69bdabe5e0bcf5d
Ian Wu - IBM <onlinemad@gmail.com>
2012-12-11 17:35:14 +0800

    Add xml2js to parser xml that recive from server.

--- test/ckipclient.send.js
+++ test/ckipclient.send.js
@@ -3,8 +3,9 @@ var CKIPClient = require('../');
 describe('ckipclient', function(){
 	describe('.send()', function(){
 		var client = new CKIPClient('127.0.0.1', 9999, 'user', 'pwd');
+		client.open();
 		it('should send text to CKIP server', function(done){
-			client.send('台新金控12月3日將召開股東臨時會進行董監改選。');
+			client.send('台新金控12月3日將召開股東臨時會進行董監改選。', done);
 		})
 	})
 })
